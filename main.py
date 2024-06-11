25644cf587cfd588070bfc54eb38199e6c5bb57a
kfinn315 <kfinn315@gmail.com>
2024-06-11 08:25:43 -0400

    Rename api.py to main.py

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ import numpy
 app = Flask(__name__)
 CORS(app)
 
+
 def patch_asscalar(a):
     return a.item()
 
+
 setattr(numpy, "asscalar", patch_asscalar)
+
 
 def roundDeltaE(deltaE):
     return round(deltaE, 2)
+
 
 @app.route('/api/paint', methods=['POST'])
 def paint():
@@ -34,8 +38,10 @@ def paint():
     con.close()
     return result
 
-def tupleToDict(colnames, tuple ):
-    return {colnames[i]:tuple[i] for i in range(len(tuple))}
+
+def tupleToDict(colnames, tuple):
+    return {colnames[i]: tuple[i] for i in range(len(tuple))}
+
 
 @app.route('/api/paint_search', methods=['POST'])
 def paint_search():
@@ -61,10 +67,12 @@ def paint_search():
             continue
 
         if delta_number < range:
-            paint_results.append({"paint": tupleToDict(colnames, paint), "delta": roundDeltaE(delta_number)})
+            paint_results.append({"paint": tupleToDict(
+                colnames, paint), "delta": roundDeltaE(delta_number)})
     result = jsonify(message=paint_results)
     # print(paint_results)
     return result
+
 
 @app.route('/api/delta', methods=['POST'])
 def delta():
@@ -76,4 +84,4 @@ def delta():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0", port=5000)
